e480549e6f08214bc072b8d3115e0da41561c3b4
SurisettyGit <surisetty08@gmail.com>
2024-05-31 21:13:45 +0530

    expense project

--- 04-db/main.tf
+++ 04-db/main.tf
@@ -15,7 +15,7 @@ module "db" {
   vpc_security_group_ids = [data.aws_ssm_parameter.db_sg_id.value]
 
   # DB subnet group
-  db_subnet_group_name         = data.aws_ssm_parameter.db_subnet_group_name.value
+  db_subnet_group_name = data.aws_ssm_parameter.db_subnet_group_name.value
 
   # DB parameter group
   family = "mysql8.0"
@@ -23,16 +23,16 @@ module "db" {
   # DB option group
   major_engine_version = "8.0"
 
-    
+
   tags = merge(
     var.common_tags,
     {
-        Name = "${var.project_name}-${var.env}"
+      Name = "${var.project_name}-${var.env}"
     }
   )
 
   manage_master_user_password = false
-  password = "ExpenseApp1"
+  password                    = "ExpenseApp1"
 
   skip_final_snapshot = true
 
@@ -63,6 +63,23 @@ module "db" {
       ]
     },
   ]
+}
 
+# create r53 record for RDS endpoint
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
 
+  zone_name = var.zone_name
+
+  records = [
+    {
+      name = "db"
+      type = "CNAME"
+      ttl  = 1
+      records = [
+        module.db.db_instance_address
+      ]
+    },
+  ]
 }
